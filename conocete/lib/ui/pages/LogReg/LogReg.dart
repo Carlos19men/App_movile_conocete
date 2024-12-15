b7437d29d5bef7d8658958342dd15dd48bb65bd0
Alfonso José Palma Alvarado <sjalvarado08@hotmail.com>
2024-12-14 20:55:22 -0400

    Agregar las pantallas

--- conocete/lib/ui/pages/LogReg/LogReg.dart
+++ conocete/lib/ui/pages/LogReg/LogReg.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../home/Home.dart';
 
-class Test extends StatelessWidget {
-  const Test({super.key});
+class LogReg extends StatelessWidget {
+  const LogReg({super.key});
 
   @override
   Widget build(BuildContext context) {
